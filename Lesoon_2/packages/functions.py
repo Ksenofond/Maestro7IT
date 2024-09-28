598130c65946dd80e0cc9290ff1021a758f39559
Ksenofond <egorov7ya.2@gmail.com>
2024-09-28 15:15:10 +0300

    28.09.2024

---
+++ Lesoon_2/packages/functions.py
@@ -0,0 +1,18 @@
+'''
+Cоздание пакета калькулятора с модулями (функциями)
+'''
+
+def add(a, b):
+    return print("Результат сложения:", a + b)
+
+def minus(a, b):
+    return print("Результат вычитания:", a -b)
+
+def multy(a, b):
+    return print("Результат умножения:", a * b)
+
+def divide(a, b):
+    return print("Результат деления:", a / b)
+
+def multyply(a, b):
+    return print("Результат возведения в степень:", a ** b)
---
+++ Lesoon_2/packages/main.py
@@ -0,0 +1,7 @@
+from functions import add, minus, multy, divide, multyply
+
+add(5, 3)
+minus(5, 3)
+multy(5, 3)
+divide(5, 3)
+multyply(5, 3)
